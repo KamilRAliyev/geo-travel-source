498e7acf4624845d79d5e22207b409c50ceff95f
Kamil Aliyev <kamilraliyev@gmail.com>
2021-10-23 11:32:07 +0300

    [+] TourCategory model tests

--- tours/tests/test_models.py
+++ tours/tests/test_models.py
@@ -1,1 +1,12 @@
-from django.test import testcases
+from django.test import TestCase
+import tours.models as models 
+
+class TestTourModels(TestCase):
+    def test_TourCategory_str(self):
+        title = models.TourCategory.objects.create(name="test category", order=1)
+        self.assertEqual(str(title), "test category")
+
+    def test_TourCategory_urlgen(self):
+        object = models.TourCategory.objects.create(name="test1")
+        self.assertEqual(f"/tours/category/{object.pk}", object.get_absolute_url())
+
