7723b5ccd4cb08b412d137ca0ac6b67d67f19608
Kamil Aliyev <kamilraliyev@gmail.com>
2021-10-23 11:49:11 +0300

    [*] Coverage test

--- tours/tests/test_models.py
+++ tours/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 import tours.models as models 
+from . import utils
 
 class TestTourModels(TestCase):
     def test_TourCategory_str(self):
@@ -10,3 +11,14 @@ class TestTourModels(TestCase):
         object = models.TourCategory.objects.create(name="test1")
         self.assertEqual(f"/tours/category/{object.pk}", object.get_absolute_url())
 
+    def test_Tour_str(self):
+        category = models.TourCategory.objects.create(name="test_category", order=1)
+        tour = models.Tour.objects.create(name="tour1", description="none", price=9.99, duration=5, city="Baku", category=category)
+
+        self.assertEqual(str(tour), f"{tour.name} | {tour.description}")
+    
+    def test_Tour_urlgen(self):
+        category = models.TourCategory.objects.create(name="test_category", order=1)
+        tour = models.Tour.objects.create(name="tour1", description="none", price=9.99, duration=5, city="Baku", category=category)
+        self.assertEqual(f'/tours/tour/{tour.slug}', tour.get_absolute_url())
+    
